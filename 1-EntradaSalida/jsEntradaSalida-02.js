bdf2640ab5db8e888b801cbf300e376563f6270c
iantorres19 <86858545+iantorres19@users.noreply.github.com>
2021-07-05 22:45:38 -0300

    Add files via upload
    nuevos cambios

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -2,7 +2,7 @@
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar() {
-	let nombre;
-	nombre = prompt("ingrese su nombre");
-	alert(nombre);
+	var nombreDeUsuario;
+	nombreDeUsuario=prompt("dato a tomar");
+	alert(nombreDeUsuario);
 }
--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -3,7 +3,11 @@ Debemos lograr tomar un dato por 'PROMPT'
 y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
-{
-	alert("ok");
+{ var nombre;
+	var mensaje;
+	nombre=prompt("ingrese un nombre");
+	mensaje="su nombre es:" + nombre;
+	nombreIngresado=document.getElementById("txtIdNombre").value=mensaje;
+	
 }
 
