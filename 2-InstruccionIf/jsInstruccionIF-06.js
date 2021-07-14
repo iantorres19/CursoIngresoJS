fcb878bec46a7cc57b3ae89447b9f754e1797e95
iantorres19 <86858545+iantorres19@users.noreply.github.com>
2021-07-13 23:00:14 -0300

    Add files via upload
    nuevos cambios

--- 2-InstruccionIf/jsInstruccionIF-06.js
+++ 2-InstruccionIf/jsInstruccionIF-06.js
@@ -1,15 +1,21 @@
-function mostrar()
-{
+/*Enunciado:
+Al ingresar una edad debemos informar si la persona es mayor de edad (mas de 18 años) o adolescente (entre 13 y 17 años) o niño (menor a 13 años).autor: ian torres
+*/
+function mostrar() {
 	//tomo la edad
 	var edad;
-	edad=document.getElementById("txtIdEdad").value;  
-	if(edad>=18 ||edad>=13 && edad<=17 || edad>13 )
-{
-	alert("mayor de edad");
-	alert("adolecente");
-	alert("niño");
-}	
+	edad = document.getElementById("txtIdEdad").value;
+	if (edad >= 18) {
+		alert("mayor de edad");
+	}
+	else if(edad >= 13 && edad <= 17)
+	{
 
+		alert("adolecente");
 
-
-}//FIN DE LA FUNCIÓN
+	}
+	if (edad < 13) {
+		alert("niño");
+	}
+	//FIN DE LA FUNCIÓN
+}
--- 2-InstruccionIf/jsInstruccionIF-08.js
+++ 2-InstruccionIf/jsInstruccionIF-08.js
@@ -1,8 +1,19 @@
-function mostrar()
-{
+/*Enunciado:
+Al ingresar una edad menor a 18 años y un estado civil distinto a "Soltero", NO HACER NADA,
+pero si no es asi, y es soltero y no es menor, mostrar el siguiente mensaje: 'Es soltero y no es menor.'
+autor: ian torres
+*/
+function mostrar() {
 	//tomo la edad  
-	alert("ok");
-	
+	var edad;
+	var estado;
 
+	edad = document.getElementById("txtIdEdad").value;
+	estado = document.getElementById("estadoCivil").value;
+
+	if ((edad >= 18) && (estado == "Soltero")) {
+
+		alert("Es soltero y no es menor");
+	}
 
 }//FIN DE LA FUNCIÓN
