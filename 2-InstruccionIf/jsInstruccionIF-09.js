060422d80718c68fceadc5da24fc7951bf202099
iantorres19 <86858545+iantorres19@users.noreply.github.com>
2021-07-14 22:37:09 -0300

    Add files via upload
    ..

--- 2-InstruccionIf/jsInstruccionIF-09.js
+++ 2-InstruccionIf/jsInstruccionIF-09.js
@@ -1,6 +1,12 @@
-function mostrar()
-{
+/*Enunciado:
+Al presionar el Botón, mostrar un número Random del 1 al 10 inclusive.autor: ian torres
+*/
+function mostrar() {
 	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");	
+	var numero;
+	var numeroMaximo = 10;
+	var numeroMinimo = 1;
+	numero = Math.round(Math.random() * (numeroMaximo - numeroMinimo) + numeroMinimo);
+	alert(numero);
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,6 +1,31 @@
-function mostrar()
-{
+/*Enunciado:
+Al presionar el Botón, asignar una nota RANDOM al examen y mostrar:
+"EXCELENTE" para notas igual a 9 o 10,
+"APROBÓ" para notas mayores a 4,
+"Vamos, la proxima se puede" para notas menores a 4
+autor: ian torres
+*/
+function mostrar() {
 	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");
+	var numero;
+
+	var numeroMaximo = 10;
+	var numeroMinimo = 1;
+	numero = Math.round(Math.random() * (numeroMaximo - numeroMinimo) + numeroMinimo);
+	if (numero >= 9) {
+		alert(" excelente,nota " + numero);
+	}
+	else {
+		if (numero >= 5) {
+			alert(" aprobo, nota  " + numero);
+		}
+		else {
+			if (numero <= 4) {
+				alert(" vamos, la proxima se puede, nota " + numero);
+			}
+		}
+	}
+
+
 
 }//FIN DE LA FUNCIÓN
